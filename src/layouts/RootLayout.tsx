77ff13eaacb95b3493c86cac6375b4186cbd9c74
annFromEarth <dv_anna@mail.ru>
2023-11-05 21:36:31 +0300

    refactor: move ErrorBoundary inside router scope

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,20 +1,23 @@
 import { Link, Outlet } from 'react-router-dom';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
+import ErrorBoundary from '../components/ErrorBoundary/ErrorBoundary';
 
 import styles from './rootLayout.module.css';
 
 export default function RootLayout() {
   return (
     <div className={styles.root__layout}>
-      <Header />
-      <main className={styles.main__wrapper}>
-        <Link to="search/1" className={styles.main__link}>
-          Explore our planet search!
-        </Link>
-        <Outlet />
-      </main>
-      <Footer />
+      <ErrorBoundary>
+        <Header />
+        <main className={styles.main__wrapper}>
+          <Link to="search/1" className={styles.main__link}>
+            Explore our planet search!
+          </Link>
+          <Outlet />
+        </main>
+        <Footer />
+      </ErrorBoundary>
     </div>
   );
 }
